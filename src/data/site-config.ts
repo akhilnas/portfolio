835d6a1f2e61e6c5cc44a4ad4f829cfc00a5287f
akhilnas <eakhil.n@gmail.com>
2024-09-01 16:58:27 +0200

    Updated Home Page

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -38,8 +38,8 @@ export type SiteConfig = {
 };
 
 const siteConfig: SiteConfig = {
-    title: 'Dante',
-    subtitle: 'Minimal Astro.js theme',
+    title: '',
+    subtitle: '',
     description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
     image: {
         src: '/dante-preview.jpg',
@@ -101,7 +101,7 @@ const siteConfig: SiteConfig = {
     ],
     hero: {
         title: 'Hi There & Welcome to My Personal Homepage!',
-        text: "I'm **Akhil Nasser**, a Data Scientist at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
+        text: "I'm **Akhil Nasser**, a Data Scientist at Innoplexus, with comprehensive experience in managing the entire lifecycle of Data Science projects, with a strong focus on solution design and implementation. Feel free to explore some of my coding endeavors on <a href='https://github.com/akhilnas/'>GitHub</a>.",
         image: {
             src: '/portfolio/hero.jpeg',
             alt: 'A person sitting at a desk in front of a computer'
